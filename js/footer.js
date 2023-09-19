0daca49a25583051b67a349e382ca066380947b9
Lea Henckel <lea.henckel@outlook.com>
2023-09-19 09:37:23 +0200

    Class name changes to footer

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 const footer = document.querySelector(".site__footer")
 footer.innerHTML =`
 <footer class="site__footer">
-    <article class="flex__container">
+    <article class="flex__container__footer">
         <section class="flex__items1">
             <p class="footer__paragh">Hi-Fi Corner</p>
             <p class="footer__paragh">Edinburgh 2 Joppa Road EH15 2EU</p>
