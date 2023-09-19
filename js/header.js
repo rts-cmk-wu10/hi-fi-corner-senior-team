a3b59c3433f97c5124f6a85baa3069558fa65651
Aya <ayachristia@gmail.com>
2023-09-19 08:19:31 +0200

    Opdateret header, index

---
+++ js/header.js
@@ -0,0 +1,21 @@
+const HEADER = document.querySelector("header")
+HEADER.innerHTML = `
+<section class="header__top">
+<i class="fa-solid fa-location-dot"></i>
+<i class="fa-solid fa-phone"></i>
+<i class="fa-regular fa-envelope"></i>
+<button class="subscribe__btn">Subscribe</button>
+</section>
+
+<section class="header__navbar">
+<nav>
+<a href="index.html">home</a>
+<a href="#">about us</a>
+<a href="branch.html">brands</a>
+<a href="#">blog</a>
+<a href="#">events</a>
+<a href="categoryList.html">shop</a>
+<a href="contact.html">contact us</a>
+</nav>
+</section>
+`
