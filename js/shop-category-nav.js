d208b650bcb614178720633470d52c625dba1590
jondee <krbonlokke@gmail.com>
2023-09-20 22:08:31 +0200

    add categoryList js & sortBy option

--- js/shop-category-nav.js
+++ js/shop-category-nav.js
@@ -1,10 +1,8 @@
-// Get references to DOM elements
 const contentContainer = document.getElementById("shop__categoryContainer");
-const navLinks = document.querySelectorAll(".categoryNav__navItem a"); // Select the <a> elements inside .categoryNav__navItem
+const navLinks = document.querySelectorAll(".categoryNav__navItem a"); 
 
-// Function to update content based on clicked link
+
 function navigateToPage(page) {
-  // Use Fetch API to load the content of the HTML file from the "categoryPages" folder
   fetch(`categoryPages/${page}.html`)
     .then(response => response.text())
     .then(html => {
@@ -15,14 +13,12 @@ function navigateToPage(page) {
     });
 }
 
-// Attach event listeners to navigation links
 navLinks.forEach(link => {
   link.addEventListener("click", event => {
-    event.preventDefault(); // Prevent default link behavior
-    const page = link.getAttribute("data-point"); // Get the attribute from the link, not the event target
+    event.preventDefault(); 
+    const page = link.getAttribute("data-point"); 
     navigateToPage(page);
   });
 });
 
-// Initialize the default page
-navigateToPage("amplifiers"); // You can set the default page here
+navigateToPage("amplifiers"); 
