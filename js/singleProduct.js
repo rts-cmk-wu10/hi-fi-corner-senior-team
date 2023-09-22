004d0f14eb0425727be33079761dd819ed5cfec0
jondee <krbonlokke@gmail.com>
2023-09-22 13:12:51 +0200

    add singleProductPage changes to master (sorry!)

--- js/singleProduct.js
+++ js/singleProduct.js
@@ -1,17 +1,30 @@
-fetch ('http://localhost:3000/products/1')
-.then(function (response) {
-        return response.json()
-})
-.then(function(data) {
-     console.log(data)
+fetch('http://localhost:3000/products/1')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const PRODUCT_IMAGE = document.querySelector(".productImage");
+    const PRODUCT_NAME = document.querySelector(".productName");
+    const PRODUCT_OTHERS = document.querySelector(".otherProducts__p")
+    const PREVIOUS_PRICE = document.querySelector(".previousPrize")
+    const CURRENT_PRICE = document.querySelector(".currentPrize")
 
-    const test = document.querySelector(".producImage")
-    console.log(data)
+    console.log(data.img);
 
-    productImage.innerHTML = `
-    <img class="productImage" src="${data.img}">
-    `
-})
+    PRODUCT_IMAGE.src = data.img;
+    PRODUCT_NAME.innerHTML = `<h2 class="productName">${data.model}</h2>`
+    PRODUCT_OTHERS.innerHTML = `<p>See other ${data.brand} products</p>`
+    PREVIOUS_PRICE.innerHTML = `<span>${data.previous_price}</span>`
+    CURRENT_PRICE.innerHTML = `<span>${data.currrent_price}</span>`
+
+   
+    
+
+  })
+  .catch(function (error) {
+    console.error(error.message);
+    // You can handle the error message here in a more user-friendly way.
+  });
 
 
   /*.then(function (response) {
