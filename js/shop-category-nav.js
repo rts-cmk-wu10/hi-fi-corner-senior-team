bd35c4197778cf181d60fdc14034f1ffb019b1cc
Aya <ayachristia@gmail.com>
2023-09-26 13:07:13 +0200

    categoryList styling

--- js/shop-category-nav.js
+++ js/shop-category-nav.js
@@ -3,6 +3,7 @@ const navLinks = document.querySelectorAll(".categoryNav__navItem");
 
 
 function navigateToPage(page) {
+
   fetch(`categoryPages/${page}.html`)
     .then(response => response.text())
     .then(html => {
@@ -11,11 +12,13 @@ function navigateToPage(page) {
     .catch(error => {
       console.error("Error loading page:", error);
     });
+
 }
 
 navLinks.forEach(link => {
   link.addEventListener("click", event => {
     event.preventDefault(); 
+
     const page = link.getAttribute("data-point"); 
     navigateToPage(page);
   });
